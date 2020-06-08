0e8b7731552252d941b9cbc1c36811de462b1531
sridharangopal <sridharangopal@users.noreply.github.com>
2020-06-08 18:21:40 -0400

    Create _SimpleConfig.ps1

---
+++ _SimpleConfig.ps1
@@ -0,0 +1,35 @@
+
+
+#disabling services
+Set-Service XblGameSave -StartupType Disabled -ErrorAction SilentlyContinue
+Set-Service XblAuthManager -StartupType Disabled -ErrorAction SilentlyContinue
+Set-Service WpnService -StartupType Disabled -ErrorAction SilentlyContinue
+Set-Service icssvc -StartupType Disabled -ErrorAction SilentlyContinue
+Set-Service wisvc -StartupType Disabled -ErrorAction SilentlyContinue
+Set-Service FrameServer -StartupType Disabled -ErrorAction SilentlyContinue
+Set-Service AudioEndpointBuilder -StartupType Disabled -ErrorAction SilentlyContinue
+Set-Service Audiosrv -StartupType Disabled -ErrorAction SilentlyContinue
+Set-Service WalletService -StartupType Disabled -ErrorAction SilentlyContinue
+Set-Service upnphost -StartupType Disabled -ErrorAction SilentlyContinue
+Set-Service TabletInputService -StartupType Disabled -ErrorAction SilentlyContinue
+Set-Service OneSyncSvc -StartupType Disabled -ErrorAction SilentlyContinue
+Set-Service WiaRpc -StartupType Disabled -ErrorAction SilentlyContinue
+Set-Service SSDPSRV -StartupType Disabled -ErrorAction SilentlyContinue
+Set-Service ScDeviceEnum -StartupType Disabled -ErrorAction SilentlyContinue
+Set-Service SensorService -StartupType Disabled -ErrorAction SilentlyContinue
+Set-Service SensrSvc -StartupType Disabled -ErrorAction SilentlyContinue
+Set-Service SensorDataService -StartupType Disabled -ErrorAction SilentlyContinue
+Set-Service RmSvc -StartupType Disabled -ErrorAction SilentlyContinue
+Set-Service QWAVE -StartupType Disabled -ErrorAction SilentlyContinue
+Set-Service PcaSvc -StartupType Disabled -ErrorAction SilentlyContinue
+Set-Service PhoneSvc -StartupType Disabled -ErrorAction SilentlyContinue
+Set-Service NcbService -StartupType Disabled -ErrorAction SilentlyContinue
+Set-Service wlidsvc -StartupType Disabled -ErrorAction SilentlyContinue
+Set-Service lltdsvc -StartupType Disabled -ErrorAction SilentlyContinue
+Set-Service SharedAccess -StartupType Disabled -ErrorAction SilentlyContinue
+Set-Service lfsvc -StartupType Disabled -ErrorAction SilentlyContinue
+Set-Service MapsBroker -StartupType Disabled -ErrorAction SilentlyContinue
+Set-Service dmwappushservice -StartupType Disabled -ErrorAction SilentlyContinue
+Set-Service CDPUserSvc -StartupType Disabled -ErrorAction SilentlyContinue
+Set-Service bthserv -StartupType Disabled -ErrorAction SilentlyContinue
+Set-Service AxInstSV -StartupType Disabled -ErrorAction SilentlyContinue
